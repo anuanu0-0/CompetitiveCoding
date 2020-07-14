bfa1732f666ce14f8b107929ad870d48fe8066d6
Anupam <onlyprogramming000@gmail.com>
2020-07-14 20:03:47 +0530

    Stones on the table

---
+++ Codeforces/StonesontheTable.cpp
@@ -0,0 +1,69 @@
+//  Time Complexity : 0(n)
+#include <bits/stdc++.h>
+
+#define test  \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define init(arr, val) memset(arr, val, sizeof(arr))
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define loopr(i, a, b) for (int i = a; i >= b; i--)
+#define loops(i, a, b, step) for (int i = a; i < b; i += step)
+#define looprs(i, a, b, step) for (int i = a; i >= b; i -= step)
+#define ull unsigned long long int
+#define ll long long int
+#define P pair
+#define PLL pair<long long, long long>
+#define PII pair<int, int>
+#define PUU pair<unsigned long long int, unsigned long long int>
+#define L list
+#define V vector
+#define D deque
+#define ST set
+#define MS multiset
+#define M map
+#define UM unordered_map
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define MM multimap
+#define F first
+#define S second
+#define IT iterator
+#define RIT reverse_iterator
+#define endl '\n'
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie();                        \
+    cout.tie();
+#define FILE_READ_IN freopen("input.txt", "r", stdin);
+#define FILE_READ_OUT freopen("output.txt", "w", stdout);
+#define all(a) a.begin(), a.end()
+#define ld long double
+using namespace std;
+
+void solve(string &str)
+{
+    char prev = str[0];
+    int count = 0;
+    ;
+    for (int i = 1; i < str.length(); i++)
+    {
+        if (str[i] == prev)
+        {
+            count++;
+        }
+        prev = str[i];
+    }
+    cout << count << endl;
+}
+
+int main()
+{
+    FAST int n;
+    string str = "";
+    cin >> n >> str;
+    solve(str);
+
+    return 0;
+}
